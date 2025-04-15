47166a939b310a370f076a3dd7f1b1635c9495aa
zenith <4b1l0901@stust.edu.tw>
2025-04-15 22:21:42 +0800

    fdsf

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,8 +1,8 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 return {
     font = wezterm.font("Cascadia Code"),
-    font_size = 13.0,
+    font_size = 20.0,
     color_scheme = "Dracula",
     enable_tab_bar = false,
     default_prog = { "/bin/zsh", "-l" },
