ddef7a0c75d3b19ff60316625f7781c712c6249c
zenith <4b1l0901@stust.edu.tw>
2025-08-29 20:46:23 +0800

    add niri-wm

---
+++ niri-wm/setup.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# Remove old configs safely
+rm -rf "$HOME/.config/niri"
+rm -rf "$HOME/.config/systemd"
+rm -rf "$HOME/.config/waybar"
+rm -rf "$HOME/.config/xdg-desktop-portal"
+rm -rf "$HOME/.config/nvim"
+
+# Recreate systemd user folder if it doesn't exist
+mkdir -p "$HOME/.config/systemd/user"
+
+# Symlink unit files first
+cp "$HOME/dotfiles/niri-wm/systemd/user/swaybg.service" "$HOME/.config/systemd/user/swaybg.service"
+cp "$HOME/dotfiles/niri-wm/systemd/user/swayidle.service" "$HOME/.config/systemd/user/swayidle.service"
+
+
+
+# Symlink config directories
+ln -s "$HOME/dotfiles/niri-wm/niri" "$HOME/.config/niri"
+ln -s "$HOME/dotfiles/waybar" "$HOME/.config/waybar"
+ln -s "$HOME/dotfiles/niri-wm/xdg-desktop-portal" "$HOME/.config/xdg-desktop-portal"
+ln -s "$HOME/dotfiles/nvim" "$HOME/.config/nvim"
+
+
+# Reload systemd so it sees the new units
+systemctl --user daemon-reload
+
+# Add dependencies for Niri
+systemctl --user add-wants niri.service mako.service
+systemctl --user add-wants niri.service waybar.service
+systemctl --user add-wants niri.service swaybg.service
+systemctl --user add-wants niri.service swayidle.service
+
+
+
